a2ef61082113398961e71bd62a5a8d69a6f36179
yadavsuprit <ay9829412@gmail.com>
2026-03-15 10:18:19 +0530

    Initial commit - FWMS Dashboard with AI & Google Maps integration

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function showAlert(message){
+alert(message)
+}
+
+function submitDonation(){
+showAlert("Donation submitted successfully!")
+}
+
+function approveRequest(){
+showAlert("Request Approved")
+}
+
+function rejectRequest(){
+showAlert("Request Rejected")
+}
